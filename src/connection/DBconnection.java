bb1d6437c263d963272d50575a478a2bb1778c73
anik112 <paulanik112@gmail.com>
2019-02-06 16:15:55 +0600

    Add New Rpo

---
+++ src/connection/DBconnection.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import com.sun.net.httpserver.Authenticator.Success;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import view_massage.*;
+
+/**
+ *
+ * @author Vista Soft IT
+ */
+public class DBconnection {
+    
+    private final static String BasedUrl="jdbc:mysql://";
+    private final static String host="localhost";
+    private final static String port="3306";
+    private static String dbName="";   
+    private final static String url=BasedUrl+host+":"+port+"/"+dbName;
+    private static Connection connection=null;
+
+    public static void setDbName(String dbName) {
+        DBconnection.dbName = dbName;
+    }
+    
+    
+    public static Connection getConnection(){   
+        
+        System.out.println(dbName);
+        
+        if(dbName.length() > 0){
+            try {
+               // Class.forName("com.mysql.jdbc.Connection");
+               connection=DriverManager.getConnection(url, "root", "");
+               if(connection != null){
+                   System.out.println("::Connected::");
+                   //new SuccessMessage("Connect.", "Successfull connection with database");
+               }
+//               System.out.println("::Connected::");
+//               new SuccessMessage("Connect","Database connected!");
+               return connection;
+            } catch (SQLException /*| ClassNotFoundException */ e) {
+               new ErrorMessage("Database Not Connect", "Please check User Name, Password, Database Name");
+               //Logger.getLogger(DBconnection.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return null;
+    }
+    
+    
+}
---
+++ src/payroll/PAYROLL.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payroll;
+import connection.DBconnection;
+import connection.DatabaseName;
+import view_massage.ErrorMessage;
+
+/**
+ *
+ * @author Vista Soft IT
+ */
+public class PAYROLL {
+    
+    
+    private static String getname ="";
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(view_massage.ErrorMessage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(view_massage.ErrorMessage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(view_massage.ErrorMessage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(view_massage.ErrorMessage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DatabaseName().setVisible(true);
+            }
+        });
+    }
+    
+}
